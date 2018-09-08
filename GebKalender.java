55dfe959e8a46c3fbaacff2b176a22ef49106ffe
Beate Brestrich <beatebre@web.de>
2018-09-08 19:39:02 +0200

    english variables

--- GebKalender.java
+++ GebKalender.java
@@ -4,12 +4,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.ActionEvent;
 
-//This is the GUI for the birthday calender.
+//This is the GUI for the birthday calender with java.awt.
 public class GebKalender extends Frame // Frame erzeugt ein eigenständiges Programmfenster
 implements WindowListener, ActionListener
 {
 	Label labText1, labText2;
-	Button bHinzu, bAendern, bLoeschen;
+	Button bAdd, bChange, bDelete;
 	TextArea taInfo;
 	
 	public static void main(String [] args)
@@ -20,9 +20,9 @@ implements WindowListener, ActionListener
 	{
 		setSize (400,400); //setzt die Fenstergröße des Frame fest
 		setLayout (null); //setLayout(Layout); legt verwendetes Layout fest, hier: kein (vorkonfiguriertes) Layout
-		Font fntFett = new Font ("SansSerif", Font.BOLD, 14); //Font.BOLD gibt Schriftstil wieder (als int), 0 = PLAIN, 1 = BOLD, 2 = ITALIC -> auch Zahleingabe möglich
+		Font fntBold = new Font ("SansSerif", Font.BOLD, 14); //Font.BOLD gibt Schriftstil wieder (als int), 0 = PLAIN, 1 = BOLD, 2 = ITALIC -> auch Zahleingabe möglich
 		Font fntHead = new Font ("SansSerif", 1, 20);
-		Font fntAusgabe = new Font ("SansSerif", 0, 12);
+		Font fntOutput = new Font ("SansSerif", 0, 12);
 		//labText1
 		labText1 = new Label ("Geburtstagskalender",Label.CENTER);
 		labText1.setFont(fntHead);
@@ -32,42 +32,42 @@ implements WindowListener, ActionListener
 		add(labText1); //add(Component c); fügt Komponente der GUI hinzu
 		//labText2
 		labText2 = new Label ("Heute haben folgende Personen Geburtstag:");
-		labText2.setFont(fntFett);
+		labText2.setFont(fntBold);
 		labText2.setBounds(20,70,350,25);
 		labText2.setBackground(Color.cyan);
 		labText2.setForeground(Color.black);
 		add(labText2);
 		//taInfo
 		taInfo = new TextArea();
-		taInfo.setFont(fntAusgabe);
+		taInfo.setFont(fntOutput);
 		taInfo.setBounds(20,115,360,140);
 		taInfo.setBackground(Color.yellow);
 		taInfo.setForeground(Color.black);
 		add(taInfo);
-		//bHinzu
-		bHinzu = new Button ("Hinzufügen");
-		bHinzu.setFont(fntFett);
-		bHinzu.setBounds(20,275,120,25);
-		bHinzu.setBackground(Color.green);
-		bHinzu.setForeground(Color.darkGray);
-		add(bHinzu);
-		bHinzu.addActionListener(this); //Events auf Button bInfo können abgerufen werden
-		//bAendern
-		bAendern = new Button ("Ändern");
-		bAendern.setFont(fntFett);
-		bAendern.setBounds(160,275,100,25);
-		bAendern.setBackground(Color.green);
-		bAendern.setForeground(Color.darkGray);
-		add(bAendern);
-		bAendern.addActionListener(this);
-		//bLoeschen
-		bLoeschen = new Button ("Löschen");
-		bLoeschen.setFont(fntFett);
-		bLoeschen.setBounds(280,275,100,25);
-		bLoeschen.setBackground(Color.green);
-		bLoeschen.setForeground(Color.darkGray);
-		add(bLoeschen);
-		bLoeschen.addActionListener(this);
+		//bAdd
+		bAdd = new Button ("Hinzufügen");
+		bAdd.setFont(fntBold);
+		bAdd.setBounds(20,275,120,25);
+		bAdd.setBackground(Color.green);
+		bAdd.setForeground(Color.darkGray);
+		add(bAdd);
+		bAdd.addActionListener(this); //Events auf Button bInfo können abgerufen werden
+		//bChange
+		bChange = new Button ("Ändern");
+		bChange.setFont(fntBold);
+		bChange.setBounds(160,275,100,25);
+		bChange.setBackground(Color.green);
+		bChange.setForeground(Color.darkGray);
+		add(bChange);
+		bChange.addActionListener(this);
+		//bDelete
+		bDelete = new Button ("Löschen");
+		bDelete.setFont(fntBold);
+		bDelete.setBounds(280,275,100,25);
+		bDelete.setBackground(Color.green);
+		bDelete.setForeground(Color.darkGray);
+		add(bDelete);
+		bDelete.addActionListener(this);
 		setVisible(true); //ohne diese Methode ist das Fenster nicht sichtbar
 		addWindowListener(this); //Events auf das Fenster können abgerufen werden
 	}
@@ -92,28 +92,28 @@ implements WindowListener, ActionListener
 	// 1 Methode aus ActionListener
 	public void actionPerformed (ActionEvent e)
 	{
-		if (e.getSource() == bLoeschen)
+		if (e.getSource() == bDelete)
 		{
-			loeschenClicked();
+			deleteClicked();
 		}
-		if (e.getSource() == bAendern)
+		if (e.getSource() == bChange)
 		{
-			aendernClicked();
+			changeClicked();
 		}
-		if (e.getSource() == bHinzu)
+		if (e.getSource() == bAdd)
 		{
-			hinzuClicked();
+			addClicked();
 		}
 	}
-	private void loeschenClicked()
+	private void deleteClicked()
 	{
 		taInfo.append("\"Löschen\" wurde geklickt.\n");
 	}
-	private void aendernClicked()
+	private void changeClicked()
 	{
 		taInfo.append("\"Ändern\" wurde geklickt.\n");
 	}
-	private void hinzuClicked()
+	private void addClicked()
 	{
 		taInfo.append("\"Hinzufügen\" wurde geklickt.\n");
 	}
